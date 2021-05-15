c77fa3e637738bf117b0c84677c85433e4adae08
XBaiLong <2995773594@qq.com>
2021-05-15 20:11:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,19 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-openclash package/luci-app-openclash
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-passwall package/luci-app-passwall
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-clash package/luci-app-clash
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-vssr package/luci-app-vssr
+git clone https://github.com/kenzok8/small package/small
+#git clone https://github.com/kenzok8/openwrt-packages package/kenzok8
+#git clone https://github.com/jerrykuku/luci-app-vssr package/luci-app-vssr
+#svn co https://github.com/281677160/openwrt-package/trunk/luci-app-argon-config package/luci-app-argon-config
+#svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-argon-config package/luci-app-argon-config
+#svn co https://github.com/281677160/openwrt-package/trunk/luci-app-filebrowser package/luci-app-filebrowser
+#svn co https://github.com/281677160/openwrt-package/trunk/luci-app-poweroff package/luci-app-poweroff
+
+git clone https://github.com/281677160/openwrt-package package/281677160
+#rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
